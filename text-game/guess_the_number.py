8c03475810f7e6da78556411f5c0e95c0c733d1e
Steve McDonald <steve.mcdonald@enginpost.com>
2020-05-20 11:22:32 -0500

    Added singular or plural messaging

--- text-game/guess_the_number.py
+++ text-game/guess_the_number.py
@@ -3,13 +3,15 @@ import random
 from os import system, name
 
 # Rules:
-#  1. The range selectable as a difficulty: easy = 1-25, medium = 1-50, hard = 1-100
+#  1. The range selectable as a difficulty:
+#     easy = 1-25, medium = 1-50, hard = 1-100
 #  2. Player gets 5 guesses to determine the number
 #  3. At each chance, the guesses are displayed and the user is told if
 #     the guess is high or low
 #  4. Player is given the opportunity to play again at the end of the game.
 #  5. Player can exit the game at any time by typing "exit"
-#  6. When the user exits, the game tells them how many times they played and the number of wins and losses
+#  6. When the user exits, the game tells them how many times
+#     they played and the number of wins and losses
 
 # global game variables
 
@@ -35,8 +37,17 @@ def clear():
 
 def setup_game():
     """ Initialize the variables at the start of a new game """
-    global user_playing, user_guesses, user_difficulty, game_number, user_won, user_games_count
+    global user_playing, user_guesses, user_difficulty
+    global user_name, game_number, user_won, user_games_count
+
     clear()
+    if user_games_count == 0:
+        user_name = input("Hello! What is your name? ")
+        print(f'Welcome, {user_name}! This is your first game.')
+    else:
+        out_message = f'Hey {user_name}, you have played {user_games_count} '
+        out_message += pluraled('game', 'games', user_games_count) + '.'
+        print(out_message)
     user_games_count += 1
     user_playing = True
     user_guesses = []
@@ -62,21 +73,24 @@ def set_difficulty(level):
     return game_dif_msg
 
 
+def pluraled(singular_text, plural_text, numeric_value):
+    """ display the right pluraled word """
+    if numeric_value == 1:
+        return singular_text
+    else:
+        return plural_text
+
+
 setup_game()
 
 while user_playing:
-    if user_games_count == 0:
-        user_name = input("Hello! What is your name? ")
-        print(f'Welcome, {user_name}! This is your first game.')
-    else:
-        print(f'Hey {user_name}, you have played {user_games_count} games.')
     input_msg = '1:Easy 2:Medium 3:Hard | Select a difficulty level: '
     input_difficulty = int(input(input_msg))
     print(set_difficulty(input_difficulty))
     print(f'OK, I have a number!')
     while len(user_guesses) < 5:
         message = ''
-        user_guesses.append(int(input('Make a guess:')))
+        user_guesses.append(int(input('Make a guess: ')))
         for user_guess in user_guesses:
             message += "Guess #" + str(user_guesses.index(user_guess)+1)
             message += " was " + str(user_guess) + "\n"
@@ -103,5 +117,8 @@ while user_playing:
 clear()
 end_message = f"Thank you for playing {user_name}. "
 end_message += f"You played {user_games_count} "
-end_message += f"games with {user_wins} wins and {user_losses} losses."
+end_message += pluraled('game', 'games', user_games_count)
+end_message += f" with {user_wins} " + pluraled('win', 'wins', user_wins)
+end_message += f" and {user_losses} "
+end_message += pluraled('loss', 'losses', user_losses) + "."
 print(end_message)
